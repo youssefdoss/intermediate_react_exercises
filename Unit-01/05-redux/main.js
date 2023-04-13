5c1310b42dcc14b0704ebdeebd863b5b47356de2
Youssef Doss <youssefdoss97@gmail.com>
2023-04-13 00:15:31 -0400

    Adding jquery logic to update faces

--- Unit-01/05-redux/main.js
+++ Unit-01/05-redux/main.js
@@ -5,10 +5,19 @@ const initialState = {
 };
 
 function rootReducer(state = initialState, action) {
+  let newState = { ...state }
   switch (action.type) {
-    case "UPDATE_FACE":
-      let newState = { ...state };
-      newState.face = action.face;
+    case "HAPPY_FACE":
+      newState.face = "✧♡(◕‿◕✿)";
+      return newState;
+    case "SAD_FACE":
+      newState.face = "(ﾟ∩ﾟ)";
+      return newState;
+    case "ANGRY_FACE":
+      newState.face = "'(ᗒᗣᗕ)՞";
+      return newState;
+    case "CONFUSED_FACE":
+      newState.face = "⚆_⚆";
       return newState;
     default:
       return state;
@@ -16,3 +25,29 @@ function rootReducer(state = initialState, action) {
 }
 
 const store = Redux.createStore(rootReducer);
+const $face = $('#face');
+
+$('#happy').on('click', function() {
+  store.dispatch({
+    type: 'HAPPY_FACE'
+  });
+  $face.text(store.getState().face);
+});
+$('#sad').on('click', function() {
+  store.dispatch({
+    type: 'SAD_FACE'
+  });
+  $face.text(store.getState().face);
+});
+$('#angry').on('click', function() {
+  store.dispatch({
+    type: 'ANGRY_FACE'
+  });
+  $face.text(store.getState().face);
+});
+$('#confused').on('click', function() {
+  store.dispatch({
+    type: 'CONFUSED_FACE'
+  });
+  $face.text(store.getState().face);
+});
